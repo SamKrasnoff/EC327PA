d52428647af96b3b4e5827c20c31e678331ccb4b
SamKrasnoff <sam.krasnoff@gmail.com>
2019-10-05 17:17:01 -0400

    updates

--- q4.cpp
+++ q4.cpp
@@ -1,15 +1,18 @@
 #include <iostream>
+#include <sstream>
 using namespace std;
 
 int main(){
     bool isNum = true;
     bool isPal = true;
     int palindrome;
+    ostringstream convert;
     string pal;
     do{
         cout << "Enter a number: " << endl;
         cin >> palindrome;
-        pal = to_string(palindrome);
+        convert << palindrome;
+        pal = convert.str();
         for (int i = 0; i < pal.length(); i++){
             if (!isdigit(pal[i])){
                 isNum = false;
